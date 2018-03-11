0baaaf1ab62dd0d509779c90edb34804b1acbbb2
zengxinagji <zengxiangji@xdf.cn>
2018-03-11 09:37:28 +0800

    add downloadExcel method

--- src/main/java/cn/zxj/utils/file/DownloadFile.java
+++ src/main/java/cn/zxj/utils/file/DownloadFile.java
@@ -1,10 +1,16 @@
 package cn.zxj.utils.file;
 
 import cn.zxj.utils.propertiy.PropertyUtil;
+import org.apache.poi.hssf.usermodel.*;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 public class DownloadFile {
 //    前端调用方式
@@ -52,4 +58,90 @@ public class DownloadFile {
             }
         }
     }
+    public void downloadExcelWithWB(HttpServletResponse response) {
+        String sheetName = "教师基本信息";
+        Set<String> header = new LinkedHashSet<String>();
+        header.add("教师姓名");
+        header.add("教师编码");
+        header.add("联系方式");
+        header.add("班级编号");
+        header.add("班级名称");
+        List<List<String>> datas = new ArrayList<List<String>>();
+
+        HSSFWorkbook wb = createWorkBook(sheetName, header, datas);
+        String excelName = "教师基本信息.xls";
+        write2Response(response, wb, excelName);
+    }
+    private HSSFWorkbook createWorkBook(String sheetName, Set<String> header, List<List<String>> datas){
+        //第一步，创建webbook，对应一个Excel文件
+        HSSFWorkbook wb = new HSSFWorkbook();
+        createBook(sheetName, header, datas, wb);
+        return wb;
+    }
+    private void createBook(String sheetName, Set<String> header,
+                            List<List<String>> datas, HSSFWorkbook wb) {
+        //第二步，在webbook中添加一个sheet,对应Excel文件中的sheet
+        HSSFSheet sheet = wb.createSheet(sheetName);
+        sheet.setDefaultColumnWidth(20);
+        //第三步，在sheet中添加表头
+        HSSFRow row = sheet.createRow(0);
+        //第四步，创建单元格，并设置居中格式
+        //设置字体
+        HSSFFont font = wb.createFont();
+        font.setFontHeightInPoints((short) 12);
+        font.setFontName("宋体");
+
+        HSSFCellStyle style = wb.createCellStyle();
+        style.setFont(font);
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER); //创建居中格式
+
+        style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
+        style.setBorderRight(HSSFCellStyle.BORDER_THIN);
+        style.setBorderTop(HSSFCellStyle.BORDER_THIN);
+
+        int cellNum = 0;
+        for(String str : header){
+            HSSFCell cell = row.createCell(cellNum);
+            cell.setCellValue(str);
+            cell.setCellStyle(style);
+            cellNum++;
+        }
+        //第五步，写入实体数据 实际应用中这些数据从数据库得到，
+        for (int i = 0; i < datas.size();i++) {
+            row = sheet.createRow(i+1);
+            for (int j = 0; j < datas.get(i).size(); j++) {
+                HSSFCell cell = row.createCell(j);
+                cell.setCellValue((String)datas.get(i).get(j));
+                cell.setCellStyle(style);
+            }
+        }
+    }
+
+    private void write2Response(HttpServletResponse response, HSSFWorkbook wb,String name) {
+        String excelName = name;
+        response.setContentType("application/vnd.ms-excel");
+        try {
+            response.setHeader("Content-Disposition", "attachment;filename="+java.net.URLEncoder.encode(excelName, "UTF-8"));
+        } catch (UnsupportedEncodingException e1) {
+            e1.printStackTrace();
+        }
+        ServletOutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            wb.write(outputStream);
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            if(null != outputStream ){
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
